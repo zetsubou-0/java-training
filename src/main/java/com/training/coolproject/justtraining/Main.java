0f57ea3c8ec8c521882cab3c6fd892c12e1b739a
Torus <valiktorus@mail.ru>
2016-11-07 20:37:53 +0200

    test

--- src/main/java/com/training/coolproject/justtraining/Main.java
+++ src/main/java/com/training/coolproject/justtraining/Main.java
@@ -3,6 +3,6 @@ package com.training.coolproject.justtraining;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test1");
     }
 }
