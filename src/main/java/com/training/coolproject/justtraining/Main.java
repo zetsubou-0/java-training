a350527c6380ff7461ab80dc9cc68cd23aa27d34
zetsubou_0 <zetsubou.zero.0@gmail.com>
2016-11-07 20:28:58 +0300

    test

--- src/main/java/com/training/coolproject/justtraining/Main.java
+++ src/main/java/com/training/coolproject/justtraining/Main.java
@@ -1,9 +1,7 @@
 package com.training.coolproject.justtraining;
 
-/**
- * Created by zetsubou_0
- */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("");
     }
