c532ed962bf68ea76ade39f225902bfbf97ba77f
Kiryl Lutsyk <kiryl.lutsyk@axamit.com>
2016-11-14 11:05:42 +0300

    app structure

---
+++ src/main/java/com/training/coolproject/justtraining/App.java
@@ -0,0 +1,7 @@
+package com.training.coolproject.justtraining;
+
+public class App {
+    public static void main(String[] args) {
+        // run application
+    }
+}
---
+++ src/main/java/com/training/coolproject/justtraining/ui/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by kiryl.lutsyk on 14.11.2016.
+ *
+ * Code is related to user interface
+ */
+package com.training.coolproject.justtraining.ui;
